5a6201b296fe6118b4ad81abf53cd94bf9941054
zhangjk4859 <573581325@qq.com>
2016-08-26 15:28:51 +0800

    NSOpearaionDemo

---
+++ OC-TestProhect/OC-TestProhect/线程示例/NSOperationDemo.h
@@ -0,0 +1,13 @@
+//
+//  NSOperationDemo.h
+//  OC-TestProhect
+//
+//  Created by 张俊凯 on 16/8/26.
+//  Copyright © 2016年 张俊凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSOperationDemo : UIViewController
+
+@end
---
+++ OC-TestProhect/OC-TestProhect/线程示例/NSOperationDemo.m
@@ -0,0 +1,24 @@
+//
+//  NSOperationDemo.m
+//  OC-TestProhect
+//
+//  Created by 张俊凯 on 16/8/26.
+//  Copyright © 2016年 张俊凯. All rights reserved.
+//
+
+#import "NSOperationDemo.h"
+
+@interface NSOperationDemo ()
+
+@end
+
+@implementation NSOperationDemo
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+
+@end
